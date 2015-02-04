4198954e2891d32d5dc649c10b7065d366712bda
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-02-04 15:58:02 -0800

    add ability to analyse minute data and arbitrary lookback for Bayesian estimate

--- NaiveBayes_strat1.py
+++ NaiveBayes_strat1.py
@@ -7,35 +7,136 @@
 from make_train_test import *
 import sys
 from random import choice
+from make_train_test import *
+from collections import deque
+from datetime import datetime
 
-threshold = 0.6
-risk_prcnt = 0.05
-N_tests = 10
+threshold = 0.6 # Probability threshold for acting
+risk_prcnt = 0.05 # How much of total account to risk on any one day
+N_tests = 10 # testing years
+N_lookback = 2 # time steps 
+Lambda = 5 # days
 
-def generate_test(year, first, last):
+
+def run_simulation():
+    train = open('train.csv', 'r')
+    train = train.readlines()
+    test = open('train.csv', 'r')
+    test = test.readlines()
+ 
+#####  compute training results #####
+    results = {}
+    pattern = deque([])
+    n = 0
+    last = train[n][1]
+    while last.rstrip() == '.':
+        n += 1
+        last = train[n][1]
+    last = float(last.rstrip())
+    
+    for day in train[n+1:]:
+        pair = day.split(',')
+        if pair[1].rstrip() != '.':
+            cur = float(pair[1].rstrip()) 
+            change = cur - last
+            if change != 0:
+                last = cur
+                pattern.appendleft(cmp(change,0))
+        if len(pattern) > N_lookback:
+            if tuple(pattern) in results.keys():
+                results[tuple(pattern)] += 1
+            else:
+                results[tuple(pattern)] = 1
+            pattern.pop()
+    print results
+#### run simulation ####
+
+    USD = 100
+    X = 0
+    current = deque([])
+    
+    last = test[n][1]
+    while last.rstrip() == '.':
+        n += 1
+        last = train[n][1]
+    last = float(last.rstrip())
+    
+    for day in test[n+1:]:
+        pair = day.split(',')
+        if pair[1].rstrip() != '.':
+            cur = float(pair[1].rstrip()) 
+            USD += X * cur
+            X = 0
+            change = cur - last
+            if change != 0:
+                last = cur
+                current.appendleft(cmp(change,0))
+            if len(current) > N_lookback:
+                print type((1,)+tuple(current))
+                print type( tuple(current))
+                n_rise = results.get((1,)+tuple(current[1:]))
+                n_drop = results.get((-1,)+tuple(current[1:]))
+                current.pop()    
+
+#print tuple(current)    
+                current.pop()
+ #   compute_probs(results)
+
+def compute_probs(results):
+    # p(A | B) = p(B | A) * p(A) / p(B)
+    table = {}
+    for pattern in results:
+        prev = pattern[1:]
+        rise = results[(1,)+prev]
+        drop = results[(-1,)+prev]
+#print ('%s, p_rise: %s' % (pattern, float(rise)/(rise+drop)))
+#print pattern
+#        print results[pattern]
+
+#p_rise 
+    #p(rise | last_n) = p(last_n | rise) * p(rise) / p(last_n)
+    
+
+
+def analyze():
+    pass
+def generate_test_day(first, last, EX):
     (start_year, start_month, start_day) = ( 
         choice(range(int(first[0])+1, int(last[0])-1)),
         choice(range(1,13)),
         choice(range(1,29))
         )
-    #print ('%s, %s, %s', start_year, start_month, start_day) 
-    
-    pass    
+    make_test(dt.date(start_year, start_month, start_day),
+                    dt.date(start_year+1, start_month, start_day), EX)
+
+def generate_test_min(first, last, EX):
+    print first[0:4]
+    print last[0:4]
+    (start_year, start_month, start_day, start_hour, start_min) = ( 
+        choice(range(int(first[0:4])+1, int(last[0:4])-1)),
+        choice(range(1,13)),
+        choice(range(1,29)),
+        choice(range(1,23)),
+        choice(range(1,59))
+        )
+    start = dt.datetime(start_year, start_month, start_day, start_hour, start_min) 
+    end = dt.datetime(start_year+1, start_month, start_day, start_hour, start_min) 
+    make_test(start, end, EX)
 
 if __name__ == '__main__':
     EX = sys.argv[1]
     with open(EX) as EX_data:
         raw_data = EX_data.readlines()
-    
-    (first, last) = (raw_data[0].split(','), raw_data[-1].split(','))
-    dt_range = (convert_date(first[0]), convert_date(last[0]))
-
-#    print choice(range(int(first[0].split('-')), last[0].split['-']))
+        (first, last) = (raw_data[0].split(','), raw_data[-1].split(','))
 
     data = []
     for year in range(0, N_tests):
-        data.append(generate_test(year, first[0].split('-'), last[0].split('-')))
-    # generate 10 1-year intervals (starting at a random date) for testing
+        #generate_test_day(first[0].split('-'), last[0].split('-'), EX)
+        generate_test_min(first[1], last[1], EX)
+        run_simulation()
+        analyze() 
+
+    # generate N_tests 1-year intervals (starting at a random date) for testing
     # use the rest of the data for training.
 
     # The general strategy is to decide whether to buy or sell USD_to_X on a given day. 
@@ -45,6 +146,13 @@ if __name__ == '__main__':
 
     # At time t, the feature vector is the last n exchange rate changes, binned according to size.
 
+    # The output of the A predictor is a ranked list of currencies, ranked according to the probability
+    # that the value of the currency will rise.
+
+    # The output of the B predictor is a measure for each currency of the predicted change in value. 
+
+    # Both A and B are combined to make a decision for the next day - all of the predicted least 
+    # valuable currency is 
 
     
                     #    for (test, train) in data:
--- make_train_test.py
+++ make_train_test.py
@@ -14,29 +14,46 @@ TEST_FRACT = 0.1
 def convert_date(date):
     date = date.split('-')
     date = [int(x) for x in date]
-    return dt.date(date[0], date[1], date[2])
+    if len(date) == 3:
+        return dt.date(date[0], date[1], date[2])
+    elif len(date) == 5:
+        return dt.datetime(date[0], date[1], date[2], date[3], date[4])
 
 def make_test(START, END, EX_file):
+    # This function takes an exchange rate file
+    # and start and end dt objects and generates
+    # a test / train csv pair. 
     train = open('train.csv', 'w')
     test = open('test.csv', 'w')
 
     with open(EX_file, 'r') as infile:
-        for line in infile.readlines():
-            if IS_RANDOM:
-                if random() < TEST_FRACT:
-                    test.write(line)
-                else:
-                    train.write(line)
-
-            else:
+        if 'day' in EX_file:
+            for line in infile.readlines():
                 (date, rate) = line.strip().split(',')
                 date = date.split('-')
                 date = [int(x) for x in date]
                 date = dt.date(date[0], date[1], date[2])
-                if date < START or date >= END:
-                    train.write(line)
+                if rate != '.':
+                    if date < START or date >= END:
+                        train.write(line)
+                    else:
+                        test.write(line)
+        else:
+            for line in infile.readlines():
+                words = line.strip().split(',')
+                (date, time, rate) = (words[1], words[2], words[3])
+                datetime = dt.datetime(
+                    int(date[0:4]), 
+                    int(date[4:6]),
+                    int(date[6:8]),
+                    int(time[0:2]),
+                    int(time[2:4])
+                )
+                if datetime < START or datetime >= END:
+                    train.write('%s,%s\n' % (date+time, rate))
                 else:
-                    test.write(line)
+                    test.write('%s,%s\n' % (date+time, rate))
+                    
 
 
 if __name__ == '__main__':
@@ -45,26 +62,4 @@ if __name__ == '__main__':
     (START, END) = (convert_date(x) for x in [sys.argv[2], sys.argv[3]])
    
     make_test(START, END, sys.argv[1])
-
-    train = open('train.csv', 'w')
-    test = open('test.csv', 'w')
-    
-    with open(sys.argv[1], 'r') as infile:
-        for line in infile.readlines():
-            if IS_RANDOM:
-                if random() < TEST_FRACT:
-                    test.write(line)
-                else:
-                    train.write(line)
-            
-            else:
-                (date, rate) = line.strip().split(',')
-                date = date.split('-')
-                date = [int(x) for x in date]
-                date = dt.date(date[0], date[1], date[2])
-                if date < START or date >= END:
-                    train.write(line)
-                else:
-                    test.write(line)
-                    
 
